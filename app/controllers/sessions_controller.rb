b2ef63087de171ad37435c86715aaa17d630b4b0
Katleiah Ramos <katleiahramos@gmail.com>
2018-06-21 15:46:29 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,10 @@ class SessionsController < ApplicationController
         @user = User.find_by(name: params[:user][:name])
         return head(:forbidden) unless @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
-        redirect_to 
+        redirect_to
       end
 
-      private 
+      
 
-      def user_params
-        params.require(:user).permit(:name, :password, :password_confirmation)
-      end
 
 end
